17c60dba570cf4481cde3e77c617feb35ab165ec
Joshua Devavasagam <85702895+Joshua-Devavasagam@users.noreply.github.com>
2021-06-20 01:45:42 +0530

    Add files via upload

---
+++ php/connection.php
@@ -0,0 +1,11 @@
+
+<?php
+
+$conn = mysqli_connect("localhost", "id17057910_root", "Joshua@123456", "id17057910_banking_system");
+
+if (!$conn) {
+  die("Could not connect to the database due to the following error --> " . mysqli_connect_error());
+}
+
+?>
+
